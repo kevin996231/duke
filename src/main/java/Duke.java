a6e604e9f932226171a1f4af4d5bda47797afd79
b05902128 <b05902128@ntu.edu.tw>
2019-08-26 15:10:48 +0800

    Complete feature: add

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,12 +11,17 @@ public class Duke {
         //        System.out.println("Hello from\n" + logo);
         System.out.println("    Hello! I'm Duke\n    What can I do for you?");
         Scanner input = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int task_num = 0;
         while (true) {
             String userinput = input.nextLine();
             if (userinput.equals("bye")) {
                 break;
             }
-            System.out.println("    " + userinput);
+            tasks[task_num] = userinput;
+            task_num++;
+
+            System.out.println("    added: " + userinput);
         }
         System.out.println("    Bye. Hope to see you again soon!");
 
