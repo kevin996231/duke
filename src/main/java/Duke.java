934fa7bfee4108c4ddcd729a1744a6051aa851a9
b05902128 <b05902128@ntu.edu.tw>
2019-08-27 20:03:18 +0800

    Modify userinput parsing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -31,10 +31,6 @@ public class Duke {
                 }
             } else {
                 String[] inputSplit = userInput.split("\\s+");
-                String description = inputSplit[1];
-                for (int i = 2; i < inputSplit.length - 2; i++) {
-                    description += " " + inputSplit[i];
-                }
                 if (inputSplit[0].equals("done")) {
                     String stringNumber = inputSplit[1];
                     Integer number = Integer.valueOf(stringNumber);
@@ -42,16 +38,20 @@ public class Duke {
                     print("Nice! I've marked this task as done:");
                     print("  " + tasks[number - 1].getDescription());
                 } else if (inputSplit[0].equals("todo")) {
-                    for (int i = Math.max(inputSplit.length - 2,2); i < inputSplit.length; i++) {
-                        description += " " + inputSplit[i];
-                    }
+                    String description = stringCompose(inputSplit,1,inputSplit.length - 1);
                     tasks[taskNum] = new Todo(description);
                     printAddedClass();
                 } else if (inputSplit[0].equals("event")) {
-                    tasks[taskNum] = new Event(description,inputSplit[inputSplit.length - 1]);
+                    int breakpoint = find(inputSplit,"/at");
+                    String description = stringCompose(inputSplit,1,breakpoint - 1);
+                    String time = stringCompose(inputSplit,breakpoint + 1,inputSplit.length - 1);
+                    tasks[taskNum] = new Event(description,time);
                     printAddedClass();
                 } else if (inputSplit[0].equals("deadline")) {
-                    tasks[taskNum] = new Deadline(description,inputSplit[inputSplit.length - 1]);
+                    int breakpoint = find(inputSplit,"/by");
+                    String description = stringCompose(inputSplit,1,breakpoint - 1);
+                    String time = stringCompose(inputSplit,breakpoint + 1,inputSplit.length - 1);
+                    tasks[taskNum] = new Deadline(description,time);
                     printAddedClass();
                 }
             }
@@ -61,7 +61,21 @@ public class Duke {
 
 
     }
-
+    private static int find(String[] input,String key) {
+        for (int i = 0; i < input.length; i++) {
+            if( input[i].equals(key)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    private static String stringCompose(String[] input,int start, int end) {
+        String s = input[start];
+        for (int i = start + 1; i <= end; i++) {
+            s += " " + input[i];
+        }
+        return s;
+    }
     private static void printAddedClass() {
         print("Got it. I've added this task:");
         print("  " + tasks[taskNum].getDescription());
