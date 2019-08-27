5354123116dc01df5ab86b78d071e75331528b4c
b05902128 <b05902128@ntu.edu.tw>
2019-08-28 02:19:00 +0800

    Make duke understand basic dateformat

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,43 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
 public class Deadline extends Task {
 
     protected String time;
-
+    protected Date date = null;
+    SimpleDateFormat formatter1 = new SimpleDateFormat("dd/MM/yyyy HHmm");
+    SimpleDateFormat formatter2 = new SimpleDateFormat("dd/MM/yyyy");
     public Deadline(String description, String time) {
         super(description);
         this.time = time;
+        try{
+            this.date = formatter1.parse(time);
+            System.out.println(date);
+        }
+        catch(ParseException e1){
+            try{
+                this.date = formatter2.parse(time);
+            }catch (ParseException e2){
+                System.out.println("Can't understand as date. Stored as string.");
+            }
+        }
     }
 
     @Override
     public String getDescription() {
-        return "[D]" + super.getDescription() + " (by: " + time + ")";
+        if (date == null){
+            return "[D]" + super.getDescription() + " (by: " + time + ")";
+        } else {
+            return "[D]" + super.getDescription() + " (by: " + date + ")";
+        }
     }
 
     @Override
     public String getProfile() {
-        return "D" + " | " + super.getProfile() + " | " + this.time;
+        if (date == null){
+            return "D" + " | " + super.getProfile() + " | " + this.time;
+        } else {
+            return "D" + " | " + super.getProfile() + " | " + this.date;
+        }
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,43 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
 public class Event extends Task {
 
     protected String time;
-
+    protected Date date = null;
+    SimpleDateFormat formatter1 = new SimpleDateFormat("dd/MM/yyyy HHmm");
+    SimpleDateFormat formatter2 = new SimpleDateFormat("dd/MM/yyyy");
     public Event(String description, String time) {
         super(description);
         this.time = time;
+        try{
+            this.date = formatter1.parse(time);
+        }
+        catch(ParseException e1){
+            try{
+                this.date = formatter2.parse(time);
+            }catch (ParseException e2){
+                System.out.println("    Can't understand as date. Stored as string.");
+            }
+
+        }
     }
 
     @Override
     public String getDescription() {
-        return "[E]" + super.getDescription() + " (at: " + time + ")";
+        if (date == null){
+            return "[E]" + super.getDescription() + " (by: " + time + ")";
+        } else {
+            return "[E]" + super.getDescription() + " (by: " + date + ")";
+        }
     }
 
     @Override
     public String getProfile() {
-        return "E" + " | " + super.getProfile() + " | " + this.time;
+        if (date == null){
+            return "E" + " | " + super.getProfile() + " | " + this.time;
+        } else {
+            return "E" + " | " + super.getProfile() + " | " + this.date;
+        }
     }
 }
