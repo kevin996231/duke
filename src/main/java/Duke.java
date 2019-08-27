19eac69c7273cb210ce47eedcec050c6a32dde6d
b05902128 <b05902128@ntu.edu.tw>
2019-08-27 22:12:20 +0800

    Check style and complete level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,62 +20,87 @@ public class Duke {
 
 
         while (true) {
-            String userInput = input.nextLine();
-            if (userInput.equals("bye")) {
-                break;
-            } else if (userInput.equals("list")) {
-                print("Here are the tasks in your list:");
-                for (int i = 0; i < taskNum; i++) {
-                    print(Integer.toString(i + 1) + "."
-                            + tasks[i].getDescription());
-                }
-            } else {
-                String[] inputSplit = userInput.split("\\s+");
-                if (inputSplit[0].equals("done")) {
-                    String stringNumber = inputSplit[1];
-                    Integer number = Integer.valueOf(stringNumber);
-                    tasks[number - 1].markAsDone();
-                    print("Nice! I've marked this task as done:");
-                    print("  " + tasks[number - 1].getDescription());
-                } else if (inputSplit[0].equals("todo")) {
-                    String description = stringCompose(inputSplit,1,inputSplit.length - 1);
-                    tasks[taskNum] = new Todo(description);
-                    printAddedClass();
-                } else if (inputSplit[0].equals("event")) {
-                    int breakpoint = find(inputSplit,"/at");
-                    String description = stringCompose(inputSplit,1,breakpoint - 1);
-                    String time = stringCompose(inputSplit,breakpoint + 1,inputSplit.length - 1);
-                    tasks[taskNum] = new Event(description,time);
-                    printAddedClass();
-                } else if (inputSplit[0].equals("deadline")) {
-                    int breakpoint = find(inputSplit,"/by");
-                    String description = stringCompose(inputSplit,1,breakpoint - 1);
-                    String time = stringCompose(inputSplit,breakpoint + 1,inputSplit.length - 1);
-                    tasks[taskNum] = new Deadline(description,time);
-                    printAddedClass();
-                }
-            }
+            try {
 
+                String userInput = input.nextLine();
+                if (userInput.equals("bye")) {
+                    break;
+                } else if (userInput.equals("list")) {
+                    if (taskNum == 0) {
+                        throw new DukeException("There are no tasks.");
+                    }
+                    print("Here are the tasks in your list:");
+                    for (int i = 0; i < taskNum; i++) {
+                        print(Integer.toString(i + 1) + "."
+                                + tasks[i].getDescription());
+                    }
+                } else {
+                    String[] inputSplit = userInput.split("\\s+");
+                    if (inputSplit[0].equals("done")) {
+                        String stringNumber = inputSplit[1];
+                        Integer number = Integer.valueOf(stringNumber);
+                        if (number > taskNum || number < 1) {
+                            throw new DukeException("This task doesn't exist.");
+                        }
+                        tasks[number - 1].markAsDone();
+                        print("Nice! I've marked this task as done:");
+                        print("  " + tasks[number - 1].getDescription());
+                    } else if (inputSplit[0].equals("todo")) {
+                        if (inputSplit.length == 1) {
+                            throw new DukeException("The description of a todo cannot be empty.");
+                        }
+                        String description = stringCompose(inputSplit, 1, inputSplit.length - 1);
+                        tasks[taskNum] = new Todo(description);
+                        printAddedClass();
+                    } else if (inputSplit[0].equals("event")) {
+                        int breakpoint = find(inputSplit, "/at");
+                        if (breakpoint == -1) {
+                            throw new DukeException("Wrong format: no /at when creating event task.");
+                        }
+                        String description = stringCompose(inputSplit, 1, breakpoint - 1);
+                        String time = stringCompose(inputSplit, breakpoint + 1, inputSplit.length - 1);
+                        tasks[taskNum] = new Event(description, time);
+                        printAddedClass();
+                    } else if (inputSplit[0].equals("deadline")) {
+                        int breakpoint = find(inputSplit, "/by");
+                        if (breakpoint == -1) {
+                            throw new DukeException("Wrong format: no /by when creating deadline task.");
+                        }
+                        String description = stringCompose(inputSplit, 1, breakpoint - 1);
+                        String time = stringCompose(inputSplit, breakpoint + 1, inputSplit.length - 1);
+                        tasks[taskNum] = new Deadline(description, time);
+                        printAddedClass();
+                    } else {
+                        throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                    }
+                }
+            } catch (DukeException e) {
+                print(e.message);
+            }
         }
         print("Bye. Hope to see you again soon!");
-
-
     }
+
     private static int find(String[] input,String key) {
         for (int i = 0; i < input.length; i++) {
-            if( input[i].equals(key)){
+            if (input[i].equals(key)) {
                 return i;
             }
         }
         return -1;
     }
-    private static String stringCompose(String[] input,int start, int end) {
+
+    private static String stringCompose(String[] input,int start, int end) throws DukeException {
+        if (start > end) {
+            throw new DukeException("Time or description is missing.");
+        }
         String s = input[start];
         for (int i = start + 1; i <= end; i++) {
             s += " " + input[i];
         }
         return s;
     }
+    
     private static void printAddedClass() {
         print("Got it. I've added this task:");
         print("  " + tasks[taskNum].getDescription());
--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,5 +1,6 @@
-public class DukeException extends Exception{
+public class DukeException extends Exception {
     protected String message;
+
     public DukeException(String errorMessage) {
         this.message = "OOPS!!! " + errorMessage;
     }
